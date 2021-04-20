6fc55bc39fa4c47380cb3dad1916b07fcd119112
lee junho <magho909@gmail.com>
2021-04-20 15:17:19 +0900

    feat : Update next.js practice files
    Add absolute route setting
    Add dynamic route file (detail page)

--- gorgeous/pages/fruits/index.js
+++ gorgeous/pages/fruits/index.js
@@ -1,7 +1,28 @@
-const Fruits = () => {
+import styles from 'styles/Fruits.module.css';
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+    // runs at build time. this function never runs in the browser
+    // data fetch 할 수 있는 function
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    return {
+        props: { fruits: data },
+    };
+};
+
+const Fruits = ({ fruits }) => {
     return (
         <div>
             <h1>All Fruits</h1>
+            {fruits.map((fruit) => (
+                <Link href={`fruits/${fruit.id}`} key={fruit.id}>
+                    <a className={styles.single}>
+                        <h3>{fruit.name}</h3>
+                    </a>
+                </Link>
+            ))}
         </div>
     );
 };
